f59f499015ac831f67c2b20957a2ebcb5694bfdb
DaveDevPerry <daveperry.log@gmail.com>
2022-04-13 22:49:22 +0100

    added doughnut chart for demo

--- src/components/LettersRound/StatsBarChart.js
+++ src/components/LettersRound/StatsBarChart.js
@@ -6,18 +6,19 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const data = {
-	labels: ['Three', 'Four'],
+	labels: ['Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'],
 	datasets: [
 		{
-			label: '# of Votes',
-			data: [12, 19],
+			label: '# of letters',
+			data: [12, 19, 5, 12, 7, 4, 2],
 			backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)',
+				'rgba(255, 99, 132, 0.7)',
+				'rgba(54, 162, 235, 0.7)',
+				'rgba(255, 206, 86, 0.7)',
+				'rgba(75, 192, 192, 0.7)',
+				'rgba(153, 102, 255, 0.7)',
+				'rgba(255, 159, 64, 0.7)',
+				'rgba(255, 206, 86, 0.7)',
 			],
 			borderColor: [
 				'rgba(255, 99, 132, 1)',
@@ -26,17 +27,29 @@ const data = {
 				'rgba(75, 192, 192, 1)',
 				'rgba(153, 102, 255, 1)',
 				'rgba(255, 159, 64, 1)',
+				'rgba(255, 206, 86, 1)',
 			],
-			borderWidth: 1,
+			borderWidth: 2,
 		},
 	],
+};
+
+const options = {
+	legend: {
+		labels: {
+			font: {
+				color: 'blue',
+				size: 18,
+			},
+		},
+	},
 };
 
 function StatsBarChart() {
 	return (
 		<div>
 			{/* <h1>doughnut chart</h1> */}
-			<Doughnut data={data} />
+			<Doughnut data={data} options={options} />
 		</div>
 	);
 }
